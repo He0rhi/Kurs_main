dcbbc632ee4ea7d42b75130283bd62a0a1140441
Heorhi <egooorrnok@gmail.com>
2022-05-19 01:57:52 +0300

    ff

---
+++ js/tovar.js
@@ -0,0 +1,163 @@
+var tovar =[
+{
+    name:"Игровой ноутбук Lenovo Legion 5 Pro",
+    price:"8695.0р.",
+screen:" 16.0'  2560x1600 px, IPS 165 Гц",
+notification:"2021",
+brend:"Lenovo",
+series:"Lenovo Legion 5",
+autonomy:"10ч",
+dop_info:'Дополнительная информация Изготовитель: Леново Глобал Технолоджи, 23/F., Линкольн Хаус, Тхайку-плейс, 979 Кингс Роуд, Куорри-Бей, Гонконг, Китай / Lenovo PC HK Limited, 23/F., Lincoln House, Taikoo Place, 979 Kings Road, Quarry Bay, Hong KИмпортер: Общество с ограниченной ответственностью "ТДХ ДИСТРИБЬЮШН", 223053, РБ, МИнский р-н, Боровлянский с/с, 103/3-7, пом.7-57, р-н д.Дроздово',
+garant_data:"Гарантия 2 года",
+photo:"./Photo/dell.jpg"
+},
+{
+    name:"Зарядное устройство CNAYON",
+    price:"14.0р.",
+screen:"Нет",
+notification:"2021",
+brend:"CNAYON",
+series:"CNE-CCA06B",
+autonomy:"Нет",
+dop_info:'Изготовитель: ANJI RUIBAO FURNITURE CO.,LTD, Kangshan Industrial Zone, Anji County, Zhejiang Province, China\
+Импортер: Совместное закрытое акционерное общество "АСБИС", 220118, г. Минск, ул. Машиностроителей, 29, пом. 20',
+garant_data:"Гарантия 2 года"
+},
+{
+    name:"Зарядное устройство MIRU",
+    price:"8.0р.",
+screen:"Нет",
+notification:"2021",
+brend:"MIRU",
+series:"2USB 5029",
+autonomy:"Нет",
+dop_info:'Изготовитель: №#98,2 Road GuShu Xixiang, Baoan District Shenzhen, Китай Импортер: Общество с ограниченной ответственностью "Корпоративный стандарт", 223053,Минская обл, Минский р-н, д.Боровляны, ул.40 лет Победы 27/1, ком.37',
+garant_data:"Гарантия 2 года"
+},
+
+
+
+{
+    name:"Автомобильное зарядное Deppa 8-pin",
+    price:"4.0р.",
+screen:"Нет",
+notification:"2021",
+brend:"Deppa",
+series:"8-pin (22125)",
+autonomy:"Нет",
+dop_info:'Изготовитель: Санкт-Петербург, ул. Профессора Попова, д. 38 Импортер: Общество с ограниченной ответственностью "Смарт Телеком Дистрибуция", 220138, г.Минск, пер.Липковский,12, пом.228',
+garant_data:"Гарантия 6 месяцев"
+},
+{
+    name:"Смартфон Realme 9 Pro (чёрный)",
+    price:"1199.0р.",
+screen:"6.6' 1080x2412 пикселей, IPS",
+notification:"2022 ",
+brend:"realme",
+series:"9 pro",
+autonomy:"20ч",
+dop_info:'Изготовитель: Realme Chogqing Mobile Telecommunications Corp., Ltd., №178 Yulong Avenue, Yufengshan, Yubei District, Chongqing, China. Импортер: Общество с ограниченной ответственностью "ТДХ ДИСТРИБЬЮШН", 223053, РБ, МИнский р-н, Боровлянский с/с, 103/3-7, пом.7-57, р-н д.Дроздово',
+garant_data:"Гарантия 6 месяцев"
+},
+{
+    name:"Смартфон VIVO Y21 4GB/64GB ",
+    price:"549.0р.",
+screen:"6.51' IPS 720x1600  пикселей ",
+notification:"2021",
+brend:"Vivo",
+series:"Y21",
+autonomy:"20ч",
+dop_info:'Изготовитель: Shenzhen ASHX Technology Co., Ltd, 1-6 Синьхэ Йсян, Синьнань коммунити, Пинху таун, Лун ган дистрикт, Шэньчжэнь Сити, Гуандун, Китай. Импортер: Общество с ограниченной ответственностью "Единая торговая компания", 223017, Минский р-н, Новодворский с/с, а/г Гатово, дом 5, пом. 78',
+garant_data:"Гарантия 6 месяцев"
+},
+{
+    name:"Наручные часы Skmei 1579 (серебристый)",
+    price:"115.0р.",
+screen:"Циферблат",
+notification:"2021",
+brend:"SKMEI",
+series:"1579 ",
+autonomy:"2 года",
+dop_info:'Изготовитель: Guangzhou Skmei Watch Co, "7th Floor, A Building, No.1 Community Industrial Park, No.28th Dragon tang Road, Tangge Village , Shijing Town, Baiyun District, Guangzhou, China, Китай Импортер: Общество с ограниченной ответственностью "Смарт-центр логистик", 222228, Смолевичский район, д. Шипяны, ул. Центральная 1-15/2',
+garant_data:"Гарантия 1 год"
+},
+{
+    name:"Smart-часы SAMSUNG Galaxy Watch Active 2",
+    price:"699.0р.",
+screen:"1.2' Super AMOLED",
+notification:"2022",
+brend:"SAMSUNG",
+series:"(SM-R830NSSASER)",
+autonomy:"95ч",
+dop_info:'Изготовитель: ВЬЕТНАМ, Lot I-11, D2 Road, Saigon Hi-Tech Park, Tang Nhon Phu B Ward, Thu Duc City, Ho Chi Minh CITY 70000 Импортер: Закрытое акционерное общество ПАТИО, 220036, г. Минск, пр. Дзержинского, дом № 8, 13 этаж, 1302 каб.',
+garant_data:"Гарантия 1 год"
+},
+{
+    name:"Игровой ноутбук Asus ROG Strix G15",
+    price:"3405.0р.",
+screen:"15.6' 1920x1080px IPS",
+notification:"2021",
+brend:"ASUS",
+series:"Asus ROG Strix G15",
+autonomy:"10ч",
+dop_info:'Изготовитель: Китай, №.58, First Avenue. A Zone, Kunshan Comprehensive Free Trade Zone, Kunshan, Jiangsu Импортер: Общество с ограниченной ответственностью "АйТи Дистрибуция", 223053, Республика Беларусь, Минская область, Минский район, Боровлянский с/с, 103/3-7, помещение №7-50, р-н деревни Дроздово,',
+garant_data:"Гарантия 2 года"
+},
+{
+    name:"Ноутбук Lenovo IdeaPad Gaming 3",
+    price:"3399.0р.",
+screen:"15.6' IPS",
+notification:"2021",
+brend:"Lenovo",
+series:"Lenovo IdeaPad Gaming 3",
+autonomy:"11ч",
+dop_info:'Изготовитель: Леново Глобал Технолоджи, 23/F., Линкольн Хаус, Тхайку-плейс, 979 Кингс Роуд, Куорри-Бей, Гонконг, Китай / Lenovo PC HK Limited, 23/F., Lincoln House, Taikoo Place, 979 Kings Road, Quarry Bay, Hong K Импортер: Общество с ограниченной ответственностью "ТрайдексБелПлюс", 223016,Минская обл.,Минский р-н,Новодворский с/с, 33/1,пом.8-9, район д.Большое Стиклево',
+garant_data:"Гарантия 1 год"
+},
+
+
+]
+
+var imgArr = new Array();
+
+imgArr[0]= new Image();
+imgArr[0].src='./Photo/lenovo.png';
+
+imgArr[1]= new Image();
+imgArr[1].src='./Photo/zaradka_1.jpg';
+
+imgArr[2]= new Image();
+imgArr[2].src='./Photo/zaradka_2.jpg';
+
+imgArr[3]= new Image();
+imgArr[3].src='./Photo/zaradka_3.jpg';
+
+imgArr[4]= new Image();
+imgArr[4].src='./Photo/realme9pro.jpg';
+
+imgArr[5]= new Image();
+imgArr[5].src='./Photo/vivoY21.jpg';
+
+imgArr[6]= new Image();
+imgArr[6].src='./Photo/skmei.jpg';
+
+imgArr[7]= new Image();
+imgArr[7].src='./Photo/samsung_watch.jpg';
+
+imgArr[8]= new Image();
+imgArr[8].src='./Photo/lenovo_thinkpad.jpg';
+
+imgArr[9]= new Image();
+imgArr[9].src='./Photo/dell.jpg';
+
+var Login=[];
+
+var params = new URLSearchParams(document.location.search);
+
+var ing = params.get("name");
+
+  function Main(){
+document.location.href="./main.html?name="+ing;
+  }
+
+  
